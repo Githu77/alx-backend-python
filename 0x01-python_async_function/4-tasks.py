9f5ddbc77bb49e94f1c208dd585c27193027df73
Githu77 <githubenard16@gmail.com>
2024-06-10 12:54:46 +0300

    update files

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
+'''Module 4'''
 from typing import List
 import asyncio
 task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    '''CALCULATES AVERAGE RUNTIME'''
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
     return sorted(delays)
